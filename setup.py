73ddc5900ebe023750ec8e5e09fd10b1cd658ea4
nagarh <hn533621@ohio.edu>
2025-08-07 18:53:55 -0700

    Fix license classifier warning and update Python requirements

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
